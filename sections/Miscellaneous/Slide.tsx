9d50a3def454722badf8a878b91c4fcf60a0c0aa
renan04lima <renan04lima@outlook.com>
2024-03-03 17:42:38 -0300

    feat: slide banner infinite

--- sections/Miscellaneous/Slide.tsx
+++ sections/Miscellaneous/Slide.tsx
@@ -1,4 +1,3 @@
-import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
 import { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 
@@ -16,36 +15,57 @@ export interface Props {
 }
 
 export default function Slide({ content }: Props) {
-  const slideContent = content?.map(
-    ({ srcDark, srcLight, width, height, alt, repeat = 1 }) => {
+  const slideContentDark = content?.map(
+    ({ srcDark, width, height, alt, repeat = 1 }) => {
       return (
-        <div class="flex items-center gap-x-10 mx-4">
+        <div class="flex justify-center items-center gap-x-10 mx-4 text-base-content">
           {Array(repeat).fill(0).map(() => (
             <>
-              <Image
-                class="inline dark:hidden"
-                src={srcDark || ""}
-                alt={alt || ""}
-                width={width || 40}
-                height={height || 20}
-              />
-              <Image
-                class="hidden dark:inline"
-                src={srcLight || ""}
-                alt={alt || ""}
-                width={width || 40}
-                height={height || 20}
-              />
+              <div class="flex justify-center items-center gap-1 min-w-[142.86px] lg:min-w-[200px] min-h-[40px] lg:min-h-[58px] overflow-hidden">
+                <Image
+                  class="w-[96px] lg:w-[134px] h-auto block"
+                  src={srcDark || ""}
+                  alt={alt || ""}
+                  width={width || 40}
+                  height={height || 20}
+                />
+              </div>
             </>
           ))}
         </div>
       );
     },
   );
+
+  const slideContentLight = content?.map(
+    ({ srcLight, width, height, alt, repeat = 1 }) => {
+      return (
+        <div class="flex justify-center items-center gap-x-10 mx-4 text-base-content">
+          {Array(repeat).fill(0).map(() => (
+            <>
+              <div class="flex justify-center items-center gap-1 min-w-[142.86px] lg:min-w-[200px] min-h-[40px] lg:min-h-[58px] overflow-hidden">
+                <Image
+                  class="w-[96px] lg:w-[134px] h-auto block"
+                  src={srcLight || ""}
+                  alt={alt || ""}
+                  width={width || 40}
+                  height={height || 20}
+                />
+              </div>
+            </>
+          ))}
+        </div>
+      );
+    },
+  );
+
   return (
     <div class="relative w-full overflow-hidden h-11">
-      <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-11">
-        {slideContent}
+      <div class="animate-[sliding_60s_linear_infinite] absolute top-0 left-0 flex dark:hidden flex-nowrap h-11">
+        {Array(30).fill(0).map(() => slideContentDark)}
+      </div>
+      <div class="animate-[sliding_60s_linear_infinite] absolute top-0 left-0 hidden dark:flex flex-nowrap h-11">
+        {Array(30).fill(0).map(() => slideContentLight)}
       </div>
     </div>
   );
