2ea9a766a6d54c4356e61268cb5625a349950b0f
renan04lima <renan04lima@outlook.com>
2024-03-02 21:18:22 -0300

    feat: add slide banner

--- sections/Content/Features.tsx
+++ sections/Content/Features.tsx
@@ -1,76 +1,36 @@
-import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
+import { ImageWidget } from "apps/admin/widgets.ts";
+import SlideBanner, {
+  Props as SlideBannerProps,
+} from "deco-sites/mira-site/sections/Miscellaneous/Slide.tsx";
 
-/**
- * @titleBy title
- */
-export interface Card {
-  icon?: AvailableIcons;
-  /**
-   * @format html
-   */
-  title: string;
-  text: string;
-}
 
 export interface Props {
+  /** @format html */
   title?: string;
-  cards: Card[];
+  
+  widget1?: ImageWidget
+  widget2?: ImageWidget
+
+  /** @format html */
+  description?: string;
+
+  slideBanner: SlideBannerProps
 }
-
-function FeatureCard({ icon, title, text }: Card) {
-  return (
-    <div class="feature-card group group-hover:-translate-y-3">
-      {icon && (
-        <div class="p-6 rounded-full bg-white text-[#1A1A1A]">
-          <Icon id={icon} size={48} />
-        </div>
-      )}
-      <div class="space-y-4 text-center">
-        {title && (
-          <div
-            class="text-2xl font-semibold leading-[110%]"
-            dangerouslySetInnerHTML={{ __html: title }}
-          />
-        )}
-        <p class="leading-[120%]" dangerouslySetInnerHTML={{ __html: text }} />
-      </div>
-    </div>
-  );
-}
-
-const DEFAULT_CARDS = [
-  {
-    "icon": "Discount" as AvailableIcons,
-    "text": ":)",
-    "title": "<p>Visit our coupon page!</p>",
-  },
-  {
-    "icon": "Discount" as AvailableIcons,
-    "text": ":)",
-    "title": "<p>Visit our coupon page!</p>",
-  },
-  {
-    "icon": "Discount" as AvailableIcons,
-    "text": ":)",
-    "title": "<p>Visit our coupon page!</p>",
-  },
-];
-
+ 
 export default function Features(
-  { title = "Feature", cards = DEFAULT_CARDS }: Props,
+  { title,widget1, widget2, description, slideBanner }: Props,
 ) {
   return (
-    <section class="relative bg-white text-black py-20 max-w-screen">
-      <div class="mx-6 lg:container lg:mx-auto flex justify-center items-center flex-col gap-20">
+    <section class="relative bg-white text-black max-w-screen">
+      <div class="mx-6 lg:container lg:mx-auto flex justify-center items-center flex-col py-20 gap-20">
         {title && (
           <h2 class="font-medium text-[36px] lg:text-[72px] leading-[100%] text-center max-w-4xl z-10">
-            {title}
+            {title}˜
           </h2>
         )}
-        <div class="features">
-          {cards?.map((card) => <FeatureCard {...card} />)}
-        </div>
+    
       </div>
+      <SlideBanner {...slideBanner} />
     </section>
   );
 }
--- sections/Miscellaneous/Slide.tsx
+++ sections/Miscellaneous/Slide.tsx
@@ -1,47 +1,49 @@
 import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
+import { ImageWidget } from "apps/admin/widgets.ts";
+import Image from "apps/website/components/Image.tsx";
 
 export interface SlideProps {
-  label?: string;
   repeat?: number;
-  icon?: AvailableIcons;
+  srcDark?: ImageWidget;
+  srcLight?: ImageWidget;
+  alt?: string;
+  width?: number;
+  height?: number;
 }
 
 export interface Props {
   content?: SlideProps[];
 }
 
-export default function Slide({
-  content = [
-    {
-      label: "Label",
-      repeat: 30,
-      icon: "ChevronRight",
-    },
-  ],
-}: Props) {
-  const slideContent = content?.map(({ label, icon, repeat = 1 }) => {
-    return (
-      <div class="flex items-center gap-x-10 mx-4">
-        {Array(repeat).fill(0).map(() => (
-          <>
-            <span class="text-sm font-extralight text-base-content whitespace-nowrap">
-              {label}
-            </span>
-            {icon && (
-              <Icon
-                id={icon}
-                name={icon}
-                width={24}
-                height={24}
+export default function Slide({ content }: Props) {
+  const slideContent = content?.map(
+    ({ srcDark, srcLight, width, height, alt, repeat = 1 }) => {
+      return (
+        <div class="flex items-center gap-x-10 mx-4">
+          {Array(repeat).fill(0).map(() => (
+            <>
+              <Image
+                class="inline dark:hidden"
+                src={srcDark || ""}
+                alt={alt || ""}
+                width={width || 40}
+                height={height || 20}
               />
-            )}
-          </>
-        ))}
-      </div>
-    );
-  });
+              <Image
+                class="hidden dark:inline"
+                src={srcLight || ""}
+                alt={alt || ""}
+                width={width || 40}
+                height={height || 20}
+              />
+            </>
+          ))}
+        </div>
+      );
+    },
+  );
   return (
-    <div class="bg-secondary relative w-full overflow-hidden h-11">
+    <div class="relative w-full overflow-hidden h-11">
       <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-11">
         {slideContent}
       </div>
