c6c4b9459a9b184785b69ff8cda2ab5f2ae4b85d
renan04lima <renan04lima@outlook.com>
2024-03-03 01:10:06 -0300

    fix: adjust dark mode, images and text

--- sections/Content/Features.tsx
+++ sections/Content/Features.tsx
@@ -39,23 +39,23 @@ export default function Features(
   }: Props,
 ) {
   return (
-    <section class="relative bg-white text-black max-w-screen">
+    <section class="relative bg-black dark:bg-[#FFF8E6] text-white dark:text-black max-w-screen">
       <div class="p-2 lg:px-14">
-        <div class="grid lg:grid-cols-3 mb-5 gap-3">
-          <h2
-            class="lg:col-span-2 font-medium text-4xl lg:text-5xl"
+        <div class="grid lg:grid-cols-3 pb-14 gap-3">
+          <div
+            class="lg:col-span-2 font-medium text-2xl lg:text-5xl text-white dark:text-black"
             dangerouslySetInnerHTML={{ __html: title }}
           />
-          <div class="space-x-10">
+          <div class="flex flex-grow items-center space-x-10">
             <Image
-              class="inline dark:hidden"
+              class="dark:hidden  w-20 h-10 md:w-40 md:h-20"
               src={widget1?.srcDark || ""}
               alt={widget1?.alt || ""}
               width={widget1?.width || 40}
               height={widget1?.height || 20}
             />
             <Image
-              class="hidden dark:inline"
+              class="hidden  w-20 h-10 md:w-40 md:h-20"
               src={widget1?.srcLight || ""}
               alt={widget1?.alt || ""}
               width={widget1?.width || 40}
@@ -63,14 +63,14 @@ export default function Features(
             />
 
             <Image
-              class="inline dark:hidden"
+              class="inline dark:hidden  w-20 h-10 md:w-40 md:h-20"
               src={widget2?.srcDark || ""}
               alt={widget2?.alt || ""}
               width={widget2?.width || 40}
               height={widget2?.height || 20}
             />
             <Image
-              class="hidden dark:inline"
+              class="hidden dark:inline  w-20 h-10 md:w-40 md:h-20"
               src={widget2?.srcLight || ""}
               alt={widget2?.alt || ""}
               width={widget2?.width || 40}
@@ -78,14 +78,14 @@ export default function Features(
             />
           </div>
         </div>
-        <div class="grid lg:grid-cols-2 lg:gap-8">
-          <p
-            class="text-base lg:text-2xl"
+        <div class="lg:flex lg:flex-row lg:gap-8 pb-14">
+          <div
+            class="text-white dark:text-black text-sm lg:text-xl"
             dangerouslySetInnerHTML={{ __html: description1 || "" }}
           />
 
-          <p
-            class="text-base lg:text-2xl"
+          <div
+            class="text-white dark:text-black text-sm lg:text-xl"
             dangerouslySetInnerHTML={{ __html: description2 || "" }}
           />
         </div>
