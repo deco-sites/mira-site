2ff0d5addd1ac31da3ad951ae59ab5908555dc98
renan04lima <renan04lima@outlook.com>
2024-03-04 10:43:20 -0300

    feat: desktop version of hero

--- sections/Content/Hero.tsx
+++ sections/Content/Hero.tsx
@@ -1,52 +1,43 @@
-import type { ImageWidget } from "apps/admin/widgets.ts";
-import Image from "apps/website/components/Image.tsx";
 import Icon from "deco-sites/mira-site/components/ui/Icon.tsx";
 
-export interface CTA {
-  id?: string;
-  href: string;
-  text: string;
-  variant: "Normal" | "Reverse";
+export interface WorkshopButton {
+  text?: string;
+  url?: string;
 }
 
 export interface Props {
-  /**
-   * @format html
-   */
   title: string;
   description: string;
-  image?: ImageWidget;
+  workshopButton?: WorkshopButton;
 }
 
-const PLACEMENT = {
-  left: "flex-col text-left lg:flex-row-reverse",
-  right: "flex-col text-left lg:flex-row",
-};
-
 export default function HeroFlats({
-  title = "",
-  description = "",
-  image,
+  title = "Transforme suas conversas, transforme seus resultados.",
+  description = "A MIRA escala o alto desempenho através da comunicação.",
+  workshopButton = {
+    text: "PARTICIPE DO PRÓXIMO WORKSHOP",
+    url: "https://www.miraeducacao.com.br/",
+  },
 }: Props) {
   return (
-    <section class="flex flex-col h-screen mt-[-110px] dark:bg-[#FFF8E6]">
+    <section class="flex flex-col h-screen mt-[-110px] justify-center items-center dark:bg-[#FFF8E6]">
       <div class="flex-1">
       </div>
-      <div class="min-h-[414px] px-6 pb-10">
-        <h1 class="font-black text-[2rem] lg:text-[4.5rem] leading-8 lg:leading[4.5rem] dark:text-black">
-          Transforme suas conversas, transforme seus resultados.
+      <div class="min-h-[414px] max-w-[1312px]  xl:grid xl:grid-cols-2 xl:grid-rows-2 px-6 pb-10 lg:px-16 lg:pb-0">
+        <h1 class="font-black text-[2rem] lg:text-[4.5rem] leading-8 lg:leading-[4.5rem] dark:text-black">
+          {title}
         </h1>
-        <div class="mt-6 lg:mt-0 mr-[142px]">
-          <p class="font-merriweather text-base lg:text-[1.5rem] lg:leading[2.25rem] dark:text-black">
-            A MIRA escala o alto desempenho através da comunicação.
+        <div class="row-span-2 max-w-[375px] flex items-end mt-6 xl:mt-0 mr-[142px] lg:mr-0 xl:pl-16 lg:pb-40">
+          <p class="font-merriweather text-base lg:text-[1.5rem] lg:leading-[2.25rem] dark:text-black">
+            {description}
           </p>
         </div>
-        <div class="flex items-center justify-start pt-40 lg:pt-0">
+        <div class="flex items-end justify-start pt-40 lg:pt-0 lg:pb-40">
           <a
             class="btn h-9 lg:h-14 rounded-full border-none font-light text-black text-[1rem] gap-2 bg-primary dark:bg-secondary"
-            href={"https://www.miraeducacao.com.br/"}
+            href={workshopButton.url}
           >
-            PARTICIPE DO PRÓXIMO WORKSHOP
+            {workshopButton.text}
             <Icon id="ExternalLink" class="mb-1" size={20} strokeWidth={0.01} />
           </a>
         </div>
