9f00a29cf81bbeda2db21a2dc33e643fec3292e9
renan04lima <renan04lima@outlook.com>
2024-03-05 14:57:04 -0300

    feat: add animation on accordion

--- components/daisy/Accordion.tsx
+++ components/daisy/Accordion.tsx
@@ -20,36 +20,48 @@ export default function Accordion(props: Props) {
       {children2.map((c, index) => {
         return (
           <li
-            key={index}
-            className={`flex w-[calc((100%/${len})-${len2}px)] items-start lg:mr-2 rounded-[24px] mb-2 text-black hover:cursor-pointer
-            ${
+            className={`list-none lg:mr-2 rounded-[24px] mb-2 ${
               itemVisible.value == index
                 ? "bg-primary dark:bg-secondary"
-                : "bg-[#FCD28A] dark:bg-[#F4B9AD]"
-            }
-            `}
+                : "bg-[#FCD28A] dark:bg-[#F4B9AD] flex items-center lg:items-start lg:justify-center"
+            } text-black`}
             onClick={() => (itemVisible.value = index)}
           >
-            <p
-              className={`flex justify-center items-center 
-              ${
-                itemVisible.value == index ? "px-4 py-8" : "px-6 py-3"
-              } lg:px-6 w-[66px] text-[2rem] lg:text-[3rem] font-black leading-[100%] tracking-[-3.2px] lg:leading-[140%] lg:tracking-[-4.8px]`}
+            <button
+              className={`flex items-center w-full px-6 py-3`}
             >
-              {index + 1}
-            </p>
+              <div
+                className={`${
+                  itemVisible.value == index
+                    ? "pr-4"
+                    : "flex items-center lg:items-start lg:justify-center"
+                }`}
+              >
+                <span
+                  className={`text-[2rem] lg:text-[3rem] font-black leading-[100%] tracking-[-3.2px] lg:leading-[140%] lg:tracking-[-4.8px]`}
+                >
+                  {index + 1}
+                </span>
+              </div>
+              {itemVisible.value == index && (
+                <h3 className="lg:hidden pl-6 lg:pl-12 font-black text-[1.5rem] leading-relaxed lg:text-[4rem] lg:leading-[110%]">
+                  {c.title}
+                </h3>
+              )}
+            </button>
             <div
-              className={`${
-                itemVisible.value == index ? "flex" : "hidden"
-              } flex-col px-6 py-8 lg:px-12 lg:py-16 gap-10 items-start`}
+              className={`grid 
+              overflow-hidden transition-all duration-700 ease-in-out ${
+                itemVisible.value === index
+                  ? "grid-rows-[1fr] opacity-100 ml-4 pl-8 pr-6 py-8 lg:px-12 lg:py-16 lg:pt-0 gap-10"
+                  : "grid-rows-[0fr] opacity-0 lg:w-0"
+              }`}
             >
-              <h3 className="flex items-start font-black text-[1.5rem] leading-relaxed lg:text-[4rem] lg:leading-[110%]">
-                {c.title}
-              </h3>
-              <div className="overflow-hidden transition-max-height duration-300 ease-in-out 
-              mt-5 mr-6 mb-8 max-h-[1000px]
-              ">
-                <p className="font-merriweather text-base lg:text-[1.5rem] lg:leading-9">
+              <div className="overflow-hidden">
+                <h3 className="hidden lg:block pl-6 lg:pl-0 lg:pb-10 font-black text-[1.5rem] leading-relaxed lg:text-[4rem] lg:leading-[110%]">
+                  {c.title}
+                </h3>
+                <p className="ml-6 lg:ml-0 font-merriweather text-base lg:text-[1.5rem] lg:leading-9">
                   {c.description}
                 </p>
               </div>
