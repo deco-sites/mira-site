bceaf5d9c78d14634fa27c77ce3f9ac6be3ef59d
Martin Adam <martin.viana99@gmail.com>
2024-03-07 13:36:38 -0300

    Feat: Header and Hero Changes

--- components/header/Buttons/ToggleDarkMode.tsx
+++ components/header/Buttons/ToggleDarkMode.tsx
@@ -2,16 +2,16 @@ import Icon from "$store/components/ui/Icon.tsx";
 
 export default function ToggleDarkMode() {
   return (
-    <label class="swap swap-rotate text-black font-light bg-primary dark:bg-secondary border-none rounded-full w-9 h-9 md:w-11 md:h-11 lg:w-14 lg:h-14">
+    <label class="swap swap-rotate text-black font-light bg-primary dark:bg-secondary border-none rounded-full w-[34px] h-[34px]">
       <input type="checkbox" data-toggle-darkmode />
       <Icon
         id="Sun"
-        class="swap-off fill-current w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8"
+        class="swap-off fill-current w-5 h-5 md:w-6 md:h-6"
         strokeWidth={0.01}
       />
       <Icon
         id="Moon"
-        class="swap-on fill-current w-5 h-5 md:w-6 md:h-6 lg:w-8 lg:h-8"
+        class="swap-on fill-current w-5 h-5 md:w-6 md:h-6"
         strokeWidth={0.01}
       />
     </label>
--- sections/Content/Hero.tsx
+++ sections/Content/Hero.tsx
@@ -20,24 +20,24 @@ export default function HeroFlats({
   },
 }: Props) {
   return (
-    <section class="flex flex-col h-screen mt-[-110px] justify-center items-center dark:bg-[#FFF8E6]">
-      <div class="flex-1">
-      </div>
-      <div class="min-h-[414px] max-w-[1312px]  xl:grid xl:grid-cols-5 xl:grid-rows-2 px-6 pb-10 lg:px-16 lg:pb-0">
-        <h1 class="col-span-3 font-black text-base-200 text-[2rem] lg:text-[4.5rem] leading-8 lg:leading-[4.5rem] dark:text-black">
-          {title}
-        </h1>
-        <div class="col-span-2 row-span-2 max-w-[375px] flex items-end mt-6 xl:mt-0 mr-[142px] lg:mr-0 xl:pl-16 lg:pb-40">
-          <p class="font-merriweather text-base-200 text-base lg:text-[1.5rem] lg:leading-[2.25rem] dark:text-black">
-            {description}
-          </p>
+    <section class="flex h-[calc(100vh-106px)] lg:h-[calc(100vh-82px)] justify-center items-center dark:bg-[#FFF8E6]">
+      <div class="flex flex-col h-full w-full lg:max-w-[1224px] min-[1440px]:max-w-[1440px] lg:gap-10 px-6 pb-10 lg:px-16 lg:pb-0 lg:justify-center mx-auto">
+        <div class="flex flex-col lg:w-full gap-6 lg:gap-10 h-4/5 lg:h-full justify-center lg:justify-center">
+          <h1 class="font-black text-base-200 text-[2rem] max-w-[722px] lg:text-[4.5rem] lg:w-full leading-8 lg:leading-[4.5rem] dark:text-black">
+            {title}
+          </h1>
+          <div class="w-full flex items-start lg:items-end lg:justify-end">
+            <p class="max-w-[375px] font-merriweather text-base-200 text-base lg:text-[1.5rem] lg:leading-[2.25rem] dark:text-black">
+              {description}
+            </p>
+          </div>
         </div>
-        <div class="col-span-3 flex items-end justify-start pt-40 lg:pt-0 lg:pb-40">
+        <div class="flex items-center justify-start h-1/5 lg:hidden">
           <a
-            class="btn h-9 lg:h-14 rounded-full border-none font-light text-black text-[1rem] gap-2 bg-primary dark:bg-secondary"
+            class="inline-flex items-center justify-center px-3 py-2 rounded-full border-none font-light text-black text-[13px] leading-[150%] tracking-[-0.13px] gap-2 bg-primary dark:bg-secondary"
             href={workshopButton.url}
           >
-            {workshopButton.text}
+            <p class="text-nowrap">{workshopButton.text}</p>
             <Icon id="ExternalLink" class="mb-1" size={20} strokeWidth={0.01} />
           </a>
         </div>
