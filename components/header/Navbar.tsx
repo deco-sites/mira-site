6159d5b3895fffa307afe466b5956df56218fda7
renan04lima <renan04lima@outlook.com>
2024-03-07 10:17:05 -0300

    decrease margin between texts of feature section

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -90,7 +90,6 @@ function Navbar({
             <p class="text-nowrap">{workshopButton.text}</p>
             <Icon id="ExternalLink" size={14} strokeWidth={0.01} />
           </a>
-          <ToggleDarkMode />
         </div>
       </div>
     </>
