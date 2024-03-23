be6953d573298c73c8b3a3fa365940cae42c95df
renan04lima <renan04lima@outlook.com>
2024-03-23 09:14:40 -0300

    feat: move slogan and change the alignment on the hero section

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -57,19 +57,11 @@ function Navbar({
           </a>
           <ThemeController />
         </div>
-        {
-          /* <h3 class="text-[0.813rem]  leading-[0.975rem]  font-black text-white dark:text-black">
-          {slogan != undefined ? slogan : "COMUNICAÇÃO DE ALTO DESEMPENHO"}
-        </h3> */
-        }
       </div>
 
       {/* Desktop Version */}
       <div class="hidden lg:flex border-none w-full py-6 px-16 justify-between items-center">
-        <h3 class="flex items-center justify-center text-[0.875rem] min-[1440px]:text-[1rem] leading-6 font-black text-white dark:text-black">
-          {slogan != undefined ? slogan : "COMUNICAÇÃO DE ALTO DESEMPENHO"}
-        </h3>
-        <div class="flex xl:w-1/3 justify-start -order-1">
+        <div class="flex justify-start">
           <a
             href="/"
             aria-label="Mira logo"
@@ -97,7 +89,7 @@ function Navbar({
             )}
           </a>
         </div>
-        <div class="flex xl:w-1/3 items-center justify-end gap-6">
+        <div class="flex items-center justify-end gap-6">
           <a
             class="flex flex-nowrap px-3 py-2 items-center rounded-full border-none font-light text-black text-[13px] leading-tight gap-2 hover:opacity-75 transition-opacity duration-300 hover:cursor-pointer bg-main dark:bg-sub"
             href={workshopButton.url}
