e3a91420ff66071f47a6ee35a84d2a46eee87948
renan04lima <renan04lima@outlook.com>
2024-03-31 22:49:21 -0300

    feat: change status bar color

--- static/dark.js
+++ static/dark.js
@@ -1,6 +1,16 @@
 const htmlElement = document.querySelector('html');
 const toggles = document.querySelectorAll('[data-toggle-darkmode]');
 
+// Criar um elemento meta
+var metaThemeColor = document.createElement('meta');
+
+// Definir os atributos do elemento meta
+metaThemeColor.setAttribute('name', 'theme-color');
+metaThemeColor.setAttribute('content', '#000000');
+
+// Adicionar o elemento meta ao cabeçalho da página
+document.head.appendChild(metaThemeColor);
+
 Array.from(toggles).forEach(tgl => {
     tgl.addEventListener('change', (e) => {   
         if (e.target.checked) {
