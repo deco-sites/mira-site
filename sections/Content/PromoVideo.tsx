da5d70d5155c5270ef2a23889cd0936a226a5508
Martin Adam <martin.viana99@gmail.com>
2024-04-25 19:59:09 -0300

    Ident

--- sections/Content/PromoVideo.tsx
+++ sections/Content/PromoVideo.tsx
@@ -26,19 +26,21 @@ export default function PromoVideo(
     title = "OS PILARES DO MÉTODO MIRA",
     description =
       "<p>Confira um trechinho de uma de nossas &uacute;ltimas conversas:</p>",
-    video
+    video,
   }: Props,
 ) {
   return (
     <section class="w-full bg-black dark:bg-b-300">
       <div class="flex flex-col lg:max-w-[1224px] min-[1650px]:max-w-[1440px] text-b-200 px-6 py-16 lg:px-16 lg:py-[104px] mx-auto gap-10 lg:gap-20 min-[1024px]:scale-90 min-[1650px]:scale-100">
         <div class="lg:max-w-[575px] min-[1650px]:max-w-[640px] space-y-6">
-          <h2 class="text-[1.5rem] lg:text-[2.125rem] min-[1650px]:text-[2.5rem] leading-relaxed lg:leading-[2.75rem] font-black dark:text-black"
-              dangerouslySetInnerHTML={{ __html: title }}
+          <h2
+            class="text-[1.5rem] lg:text-[2.125rem] min-[1650px]:text-[2.5rem] leading-relaxed lg:leading-[2.75rem] font-black dark:text-black"
+            dangerouslySetInnerHTML={{ __html: title }}
           />
 
-          <h4 class="font-merriweather text-base lg:text-[1.275rem] min-[1650px]:text-[1.5rem] lg:leading-[2.25rem] font-normal dark:text-black"
-              dangerouslySetInnerHTML={{ __html: description }}
+          <h4
+            class="font-merriweather text-base lg:text-[1.275rem] min-[1650px]:text-[1.5rem] lg:leading-[2.25rem] font-normal dark:text-black"
+            dangerouslySetInnerHTML={{ __html: description }}
           />
         </div>
         <div class="flex w-full">
