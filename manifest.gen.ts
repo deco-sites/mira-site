0a60835093f5ef11e1c83d11bf0a0e4e35db4caa
Martin Adam <martin.viana99@gmail.com>
2024-05-14 03:31:50 -0300

    manifest

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -42,31 +42,36 @@ import * as $$$$$$30 from "./sections/Images/BannerGrid.tsx";
 import * as $$$$$$31 from "./sections/Images/Carousel.tsx";
 import * as $$$$$$32 from "./sections/Images/ImageGallery.tsx";
 import * as $$$$$$33 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$34 from "./sections/Landing/ImageWithButton.tsx";
-import * as $$$$$$35 from "./sections/Layout/Container.tsx";
-import * as $$$$$$36 from "./sections/Layout/Flex.tsx";
-import * as $$$$$$37 from "./sections/Layout/Grid.tsx";
-import * as $$$$$$38 from "./sections/Layout/GridItem.tsx";
-import * as $$$$$$39 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$40 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$41 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$42 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$43 from "./sections/Miscellaneous/DoubleSlider.tsx";
-import * as $$$$$$44 from "./sections/Miscellaneous/Slide.tsx";
-import * as $$$$$$45 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$46 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$47 from "./sections/Product/ImageGallerySlider.tsx";
-import * as $$$$$$48 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$49 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$50 from "./sections/Product/ProductInfo.tsx";
-import * as $$$$$$51 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$52 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$53 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$54 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$55 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$56 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$57 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$58 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$34 from "./sections/Landing/Agenda.tsx";
+import * as $$$$$$35 from "./sections/Landing/Footer.tsx";
+import * as $$$$$$36 from "./sections/Landing/ImageWithButton.tsx";
+import * as $$$$$$37 from "./sections/Landing/Mentors.tsx";
+import * as $$$$$$38 from "./sections/Landing/Participants.tsx";
+import * as $$$$$$39 from "./sections/Landing/SpaceCarousel.tsx";
+import * as $$$$$$40 from "./sections/Layout/Container.tsx";
+import * as $$$$$$41 from "./sections/Layout/Flex.tsx";
+import * as $$$$$$42 from "./sections/Layout/Grid.tsx";
+import * as $$$$$$43 from "./sections/Layout/GridItem.tsx";
+import * as $$$$$$44 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$45 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$46 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$47 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$48 from "./sections/Miscellaneous/DoubleSlider.tsx";
+import * as $$$$$$49 from "./sections/Miscellaneous/Slide.tsx";
+import * as $$$$$$50 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$51 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$52 from "./sections/Product/ImageGallerySlider.tsx";
+import * as $$$$$$53 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$54 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$55 from "./sections/Product/ProductInfo.tsx";
+import * as $$$$$$56 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$57 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$58 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$59 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$60 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$61 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$62 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$63 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -112,31 +117,36 @@ const manifest = {
     "deco-sites/mira-site/sections/Images/Carousel.tsx": $$$$$$31,
     "deco-sites/mira-site/sections/Images/ImageGallery.tsx": $$$$$$32,
     "deco-sites/mira-site/sections/Images/ShoppableBanner.tsx": $$$$$$33,
-    "deco-sites/mira-site/sections/Landing/ImageWithButton.tsx": $$$$$$34,
-    "deco-sites/mira-site/sections/Layout/Container.tsx": $$$$$$35,
-    "deco-sites/mira-site/sections/Layout/Flex.tsx": $$$$$$36,
-    "deco-sites/mira-site/sections/Layout/Grid.tsx": $$$$$$37,
-    "deco-sites/mira-site/sections/Layout/GridItem.tsx": $$$$$$38,
-    "deco-sites/mira-site/sections/Links/LinkTree.tsx": $$$$$$39,
-    "deco-sites/mira-site/sections/Links/Shortcuts.tsx": $$$$$$40,
-    "deco-sites/mira-site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$41,
-    "deco-sites/mira-site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$42,
-    "deco-sites/mira-site/sections/Miscellaneous/DoubleSlider.tsx": $$$$$$43,
-    "deco-sites/mira-site/sections/Miscellaneous/Slide.tsx": $$$$$$44,
-    "deco-sites/mira-site/sections/Newsletter/Newsletter.tsx": $$$$$$45,
-    "deco-sites/mira-site/sections/Product/ImageGalleryFrontBack.tsx": $$$$$$46,
-    "deco-sites/mira-site/sections/Product/ImageGallerySlider.tsx": $$$$$$47,
-    "deco-sites/mira-site/sections/Product/NotFound.tsx": $$$$$$48,
-    "deco-sites/mira-site/sections/Product/NotFoundChallenge.tsx": $$$$$$49,
-    "deco-sites/mira-site/sections/Product/ProductInfo.tsx": $$$$$$50,
-    "deco-sites/mira-site/sections/Product/ProductShelf.tsx": $$$$$$51,
-    "deco-sites/mira-site/sections/Product/ProductShelfTabbed.tsx": $$$$$$52,
-    "deco-sites/mira-site/sections/Product/SearchResult.tsx": $$$$$$53,
-    "deco-sites/mira-site/sections/Product/ShelfWithImage.tsx": $$$$$$54,
-    "deco-sites/mira-site/sections/Product/Wishlist.tsx": $$$$$$55,
-    "deco-sites/mira-site/sections/Social/InstagramPosts.tsx": $$$$$$56,
-    "deco-sites/mira-site/sections/Social/WhatsApp.tsx": $$$$$$57,
-    "deco-sites/mira-site/sections/Theme/Theme.tsx": $$$$$$58,
+    "deco-sites/mira-site/sections/Landing/Agenda.tsx": $$$$$$34,
+    "deco-sites/mira-site/sections/Landing/Footer.tsx": $$$$$$35,
+    "deco-sites/mira-site/sections/Landing/ImageWithButton.tsx": $$$$$$36,
+    "deco-sites/mira-site/sections/Landing/Mentors.tsx": $$$$$$37,
+    "deco-sites/mira-site/sections/Landing/Participants.tsx": $$$$$$38,
+    "deco-sites/mira-site/sections/Landing/SpaceCarousel.tsx": $$$$$$39,
+    "deco-sites/mira-site/sections/Layout/Container.tsx": $$$$$$40,
+    "deco-sites/mira-site/sections/Layout/Flex.tsx": $$$$$$41,
+    "deco-sites/mira-site/sections/Layout/Grid.tsx": $$$$$$42,
+    "deco-sites/mira-site/sections/Layout/GridItem.tsx": $$$$$$43,
+    "deco-sites/mira-site/sections/Links/LinkTree.tsx": $$$$$$44,
+    "deco-sites/mira-site/sections/Links/Shortcuts.tsx": $$$$$$45,
+    "deco-sites/mira-site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$46,
+    "deco-sites/mira-site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$47,
+    "deco-sites/mira-site/sections/Miscellaneous/DoubleSlider.tsx": $$$$$$48,
+    "deco-sites/mira-site/sections/Miscellaneous/Slide.tsx": $$$$$$49,
+    "deco-sites/mira-site/sections/Newsletter/Newsletter.tsx": $$$$$$50,
+    "deco-sites/mira-site/sections/Product/ImageGalleryFrontBack.tsx": $$$$$$51,
+    "deco-sites/mira-site/sections/Product/ImageGallerySlider.tsx": $$$$$$52,
+    "deco-sites/mira-site/sections/Product/NotFound.tsx": $$$$$$53,
+    "deco-sites/mira-site/sections/Product/NotFoundChallenge.tsx": $$$$$$54,
+    "deco-sites/mira-site/sections/Product/ProductInfo.tsx": $$$$$$55,
+    "deco-sites/mira-site/sections/Product/ProductShelf.tsx": $$$$$$56,
+    "deco-sites/mira-site/sections/Product/ProductShelfTabbed.tsx": $$$$$$57,
+    "deco-sites/mira-site/sections/Product/SearchResult.tsx": $$$$$$58,
+    "deco-sites/mira-site/sections/Product/ShelfWithImage.tsx": $$$$$$59,
+    "deco-sites/mira-site/sections/Product/Wishlist.tsx": $$$$$$60,
+    "deco-sites/mira-site/sections/Social/InstagramPosts.tsx": $$$$$$61,
+    "deco-sites/mira-site/sections/Social/WhatsApp.tsx": $$$$$$62,
+    "deco-sites/mira-site/sections/Theme/Theme.tsx": $$$$$$63,
   },
   "apps": {
     "deco-sites/mira-site/apps/decohub.ts": $$$$$$$$$$$0,
