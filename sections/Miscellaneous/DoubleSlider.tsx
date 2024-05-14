4a6b1349f8a0b9daa201ecb9a19da6e0650c7aae
Martin Adam <martin.viana99@gmail.com>
2024-05-14 03:31:28 -0300

    Feat: DoubleSlider

--- sections/Miscellaneous/DoubleSlider.tsx
+++ sections/Miscellaneous/DoubleSlider.tsx
@@ -19,7 +19,7 @@ export default function Slide({ title = "Organização", content }: Props) {
     { srcDark, srcLight, width, height, alt, repeat = 1 }: SlideProps,
     isDark: boolean,
   ) => (
-    <div className="flex flex-col justify-center items-center gap-x-10 mx-4 text-base-content">
+    <div className="flex flex-col justify-center items-center gap-x-10 gap-col-4 mx-4 text-base-content">
       {Array(repeat).fill(0).map((_, index) => (
         <div
           key={index}
@@ -38,7 +38,7 @@ export default function Slide({ title = "Organização", content }: Props) {
       {Array(repeat).fill(0).map((_, index) => (
         <div
           key={index}
-          className="flex justify-center items-center gap-1 min-w-[142.86px] lg:min-w-[200px] min-h-[40px] lg:min-h-[58px] overflow-hidden"
+          className="flex justify-center items-center ml-[100%] gap-1 min-w-[142.86px] lg:min-w-[200px] min-h-[40px] lg:min-h-[58px] overflow-hidden"
         >
           <img
             className="w-[96px] lg:w-[134px] h-auto block"
@@ -62,13 +62,13 @@ export default function Slide({ title = "Organização", content }: Props) {
   );
 
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <h4 class="text-b-200 dark:text-black mb-8">{title}</h4>
-      <div class="relative w-full overflow-hidden h-11">
-        <div class="animate-[sliding_60s_linear_infinite] absolute top-0 left-0 flex dark:hidden flex-nowrap h-11">
+    <div className="flex flex-col items-center justify-center gap-8">
+      <h4 class="text-b-200 dark:text-black">{title}</h4>
+      <div class="relative w-full overflow-hidden h-28">
+        <div class="animate-[sliding_60s_linear_infinite] absolute top-0 left-0 flex dark:hidden flex-nowrap">
           {Array(30).fill(0).map(() => slideContentDark)}
         </div>
-        <div class="animate-[sliding_60s_linear_infinite] absolute top-0 left-0 hidden dark:flex flex-nowrap h-11">
+        <div class="animate-[sliding_60s_linear_infinite] absolute top-0 left-0 hidden dark:flex flex-nowrap">
           {Array(30).fill(0).map(() => slideContentLight)}
         </div>
       </div>
