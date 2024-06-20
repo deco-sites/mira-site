a7f70d1f8c63f6d00b1be8db7cc3730267505745
Martin Adam <martin.viana99@gmail.com>
2024-06-20 17:09:48 -0300

    Feat: Pillar + PromoVideo

---
+++ components/ui/SectionHeading.tsx
@@ -0,0 +1,22 @@
+export interface HeadingProps {
+  /** @format rich-text */
+  title?: string;
+  subtitle?: string;
+}
+
+function SectionHeading({ title, subtitle }: HeadingProps) {
+  return (
+    <>
+      <h2
+        class="text-[1.5rem] lg:text-[3.375rem] leading-7 lg:leading-[110%] font-extrabold uppercase"
+        dangerouslySetInnerHTML={{ __html: title ?? "" }}
+      />
+
+      <p class="text-base lg:text-[1.25rem] leading-[135%] lg:leading-[150%] font-light dark:text-black">
+        {subtitle}
+      </p>
+    </>
+  );
+}
+
+export default SectionHeading;
--- sections/Content/PromoVideo.tsx
+++ sections/Content/PromoVideo.tsx
@@ -1,4 +1,5 @@
-import Accordion from "$store/islands/Accordion.tsx";
+import SectionHeading from "deco-sites/mira-site/components/ui/SectionHeading.tsx";
+import { HeadingProps } from "deco-sites/mira-site/components/ui/SectionHeading.tsx";
 
 export interface AccordionItems {
   title?: string;
@@ -7,43 +8,32 @@ export interface AccordionItems {
 }
 
 export interface Props {
-  /** @format html */
-  title?: string;
-  /** @format html */
-  description?: string;
+  heading?: HeadingProps;
   video: string;
 }
 
-const PromoExample = {
-  title: "<p>CONVERSAS DE ALTO DESEMPENHO:</p>",
-  description: "<p>Confira nossa última conversa</p>",
+const defaultHeading: HeadingProps = {
+  title: "CONVERSAS DE ALTO DESEMPENHO:",
+  subtitle:
+    "Confira nossa última conversa com Geraldo Thomaz (Co-CEO VTEX), Guilherme Rodrigues (CEO deco.cx) e Olga Loffredi (CEO Vanto Group) e descubra o impacto que comunicação de alto desempenho gerou nas suas empresas.",
 };
 
 export default function PromoVideo(
   {
-    title = PromoExample.title,
-    description = PromoExample.description,
+    heading = defaultHeading,
     video =
       "https://drive.google.com/file/d/1wrvHfqvXQBM-0T4E1LhN0sFYmP-4fGl0/preview",
   }: Props,
 ) {
   return (
     <section class="w-full bg-black dark:bg-b-300">
-      <div class="flex flex-col lg:max-w-[1224px] min-[1650px]:max-w-[1440px] text-b-200 px-6 py-16 lg:px-16 lg:py-[104px] mx-auto gap-10 lg:gap-20 min-[1024px]:scale-90 min-[1650px]:scale-100">
-        <div class="space-y-6">
-          <h2
-            class="text-[1.5rem] lg:text-[2.125rem] min-[1650px]:text-[2.5rem] leading-relaxed lg:leading-[2.75rem] font-black dark:text-black"
-            dangerouslySetInnerHTML={{ __html: title }}
-          />
-
-          <h4
-            class="font-merriweather text-base lg:text-[1.275rem] min-[1650px]:text-[1.5rem] lg:leading-[2.25rem] font-normal dark:text-black"
-            dangerouslySetInnerHTML={{ __html: description }}
-          />
+      <div class="flex flex-col lg:max-w-[1228px] min-[1650px]:max-w-[1440px] text-b-200 px-6 py-16 lg:px-16 lg:py-[104px] mx-auto gap-10 lg:gap-20 min-[1024px]:scale-90 min-[1650px]:scale-100">
+        <div class="space-y-6 text-center">
+          <SectionHeading {...heading} />
         </div>
         <div class="flex w-full justify-center">
           <iframe
-            class="w-full lg:w-4/5 aspect-video rounded border-[0.5px] border-white dark:border-0"
+            class="w-full lg:w-4/5 aspect-video rounded-xl border border-b-200 dark:border-0 p-0.5"
             src={`https://www.youtube.com/embed/${video}`}
             title="YouTube video player"
             frameborder="0"
