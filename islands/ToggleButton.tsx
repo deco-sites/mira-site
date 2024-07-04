257e6671f48cdc451bd849087450bd82b88810c7
Martin Adam <martin.viana99@gmail.com>
2024-07-04 11:31:44 -0300

    toggle fix

--- islands/ToggleButton.tsx
+++ islands/ToggleButton.tsx
@@ -8,7 +8,7 @@ function ToggleButton() {
     };
 
     return (
-        <div className="flex justify-center items-center mt-4 relative bg-transparent border border-b-200 rounded-full p-2 lg:p-3 w-full lg:man-w-[548px]">
+        <div className="flex justify-center items-center mt-4 relative bg-transparent border border-b-200 rounded-full p-2 lg:p-3 w-full lg:max-w-[548px]">
             <button
                 className={`w-1/2 px-6 py-2 lg:py-4 font-bold focus:outline-none transition duration-300 ${!displayProgram.value
                     ? "bg-main text-black rounded-full"
