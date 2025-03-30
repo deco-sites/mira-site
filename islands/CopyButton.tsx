e7ae555c104190ff730037e77bf6c95546327ea0
decobot <valls@deco.cx>
2025-03-30 17:34:06 -0300

    Refactored to use the CopyButton component in Hero and Navbar

--- islands/CopyButton.tsx
+++ islands/CopyButton.tsx
@@ -2,28 +2,43 @@ import type { Props } from "$store/components/footer/Footer.tsx";
 import Icon from "$store/components/ui/Icon.tsx";
 import { useSignal } from "@preact/signals";
 
-export default function CopyButton(
-  { contactButton = "contato@miracompany.co" }: Props,
-) {
+export interface CopyButtonProps extends Partial<Props> {
+  contactButton?: string;
+  customClass?: string;
+  iconSize?: number;
+}
+
+export default function CopyButton({
+  contactButton = "contato@miracompany.co",
+  customClass,
+  iconSize = 20,
+}: CopyButtonProps) {
   const clicked = useSignal(false);
 
   const copyToClipboard = async (text: string) => {
     try {
       await navigator.clipboard.writeText(text);
       clicked.value = true;
+      setTimeout(() => {
+        clicked.value = false;
+      }, 2000);
     } catch (err) {
       console.log("Falha ao copiar o texto", err);
     }
   };
 
+  const defaultClass =
+    "text-black h-auto border-0 flex tracking-[-0.13px] lg:tracking-[-0.16px] gap-2 rounded-[36px] px-3 lg:px-6 py-2 lg:py-4 items-center font-normal text-[13px] leading-[150%] lg:text-base bg-main dark:bg-sub uppercase";
+
   return (
     <>
       <button
-        onClick={() => copyToClipboard(contactButton as string)}
-        class="text-black h-auto border-0 flex tracking-[-0.13px] lg:tracking-[-0.16px] gap-2 rounded-[36px] px-3 lg:px-6 py-2 lg:py-4 items-center font-normal text-[13px] leading-[150%] lg:text-base bg-main dark:bg-sub uppercase"
+        type="button"
+        onClick={() => copyToClipboard(contactButton)}
+        class={customClass || defaultClass}
       >
         {!clicked.value ? contactButton : "Email copiado!"}{" "}
-        <Icon id="ExternalLink" size={20} strokeWidth={0.01} />
+        <Icon id="ExternalLink" size={iconSize} strokeWidth={0.01} />
       </button>
     </>
   );
